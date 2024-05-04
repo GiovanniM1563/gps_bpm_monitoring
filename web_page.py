da85c4757c0a34044b9e71d5a2436c7f2b2752a2
GiovanniM1563 <77891157+GiovanniM1563@users.noreply.github.com>
2024-05-03 17:56:32 -0700

    updates

--- web_page.py
+++ web_page.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import pandas as pd
-import GPSUtil as gps
+#import GPSUtil as gps
 import osmnx as ox
 import time
 import folium
-import serial as bpm
+#import transfer as bpm
 #from bpm_read import bpm_reading
 
-def webpage():
+def webpage(cur_lat, cur_long, bpm):
     """
     Display the contents of a variable and coordinates in real-time on a Streamlit webpage.
 
@@ -20,37 +20,18 @@ def webpage():
     #initialize time 
     st.session_state.start_time = time.time()
 
-    # Create a 
-    # DataFrame with the coordinates
-    cur_lat, cur_long = gps.get_gps_readout() 
+    if bpm is None:
+        BPM = "No data available"
+
+    if cur_lat is None:
+        cur_lat = "No data available"
+        cur_long = "No data available"
 
     display_df = pd.DataFrame({
         'LATITUDE': [cur_lat],
         'LONGITUDE': [cur_long]
     })
     
-    location_history = pd.DataFrame({
-        'LATITUDE': [],
-        'LONGITUDE': [],
-        "TIME": pd.Timestamp.now()
-     })
-    
-    # Create a Folium map centered at the given coordinates
-    current_location_map = folium.Map(location=[cur_lat, cur_long], zoom_start=15)
-
-    # Add a marker at the given coordinates
-    folium.Marker([cur_lat, cur_long]).add_to(current_location_map)
-
-    asset = current_location_map
-
-    # Get the BPM reading
-    BPM = bpm.get_reading()
-
-    # if three have passed
-    if time.time() - st.session_state.start_time >= 10:
-        location_history = location_history.append(display_df, ignore_index=True)
-        st.session_state.start_time = time.time()
-
     # Create a Streamlit app
     st.title("Real-time Variable and Coordinates Display")
 
@@ -68,25 +49,7 @@ def webpage():
         st.header("Current Location")
         st.write("Latitude:", cur_lat)
         st.write("Longitude:", cur_long)
-        st.write("Nearest Address:", gps.get_address(cur_lat, cur_long))
-
-
-
-    # Display the map
-    st.components.v1.html(asset.render(), width=800, height=600)
-    # if st.button is clicked, generate route
-    if st.button("Show Route"):
-        nodes = gps.get_nearest_nodes(location_history)
-        route, _, _ = gps.generate_route(nodes)
-        asset = route
-
-    if st.button("Show Current Location"):
-        asset = current_location_map
-
-
-
-
-    st.dataframe(location_history)
+        #st.write("Nearest Address:", gps.get_address(cur_lat, cur_long))
 
 
 
@@ -95,4 +58,4 @@ def webpage():
 # Example usage:
 
 
-webpage()
+webpage(None, None, None)
